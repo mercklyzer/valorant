c8d58ae1e08c3f27db42e416570f9d7df27140c2
mercklyzer <mercklyzer@gmail.com>
2022-01-20 21:43:09 +0800

    Add effect on valorant title navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './nav.css'
 import {ReactComponent as ValorantTitle} from '../../images/valorant-title.svg'
 import {ReactComponent as ValorantLogo} from '../../images/valorant-logo.svg'
@@ -13,11 +13,16 @@ const Nav = () => {
     console.log("route change");
   }
 
+  const [isMouseOver, setIsMouseOver] = useState(false)
+
   return (
     <nav className="navbar">
         <div className="navbar-items">
           <div className="navbar-item"><RiotLogo fill="orange"/></div>
-          <div className="navbar-item" onClick={routeChange} style={{cursor:'pointer'}}><ValorantTitle fill="white"/></div>
+          <div className="navbar-item" 
+            onClick={routeChange} style={{cursor:'pointer'}}>
+            <ValorantTitle fill={isMouseOver? '#ff4655' : 'white'} style={{transition: '0.4s'}} onMouseOver={() => setIsMouseOver(true)} onMouseOut={() => setIsMouseOver(false)}/>
+          </div>
           <div className="navbar-item"><ValorantLogo fill="white"/></div>
         </div>
     </nav>
