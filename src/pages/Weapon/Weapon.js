5259c0f35fec907b347c68edcb27422b9acd2936
mercklyzer <mercklyzer@gmail.com>
2022-01-21 21:08:53 +0800

    Fix import issue on useState

--- src/pages/Weapon/Weapon.js
+++ src/pages/Weapon/Weapon.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import axios from "axios";
-import { useEffect, useState } from "react/cjs/react.development";
 import Wallpaper from "../../components/Wallpaper/Wallpaper";
 import OmenWallpaper from '../../images/omen-wallpaper.jpg'
 import Section from '../../components/Section/Section'
