f7eb7a9bdaaf4b9231599d31b040c9eb38482153
mercklyzer <mercklyzer@gmail.com>
2022-01-21 19:56:49 +0800

    Add specific weapon page

--- src/pages/Weapon/Weapon.js
+++ src/pages/Weapon/Weapon.js
@@ -2,11 +2,16 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react/cjs/react.development";
 import Wallpaper from "../../components/Wallpaper/Wallpaper";
+import OmenWallpaper from '../../images/omen-wallpaper.jpg'
+import Section from '../../components/Section/Section'
+import Loader from '../../components/Loader/Loader'
 
 const Weapon = ({match}) => {
-    const [image, setImage] = useState('')
+    const [weapon, setWeapon] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         axios.get(`https://valorant-weapons.p.rapidapi.com/weapon/${match.params.weapon}`, {
             headers: {
                 'x-rapidapi-host': 'valorant-weapons.p.rapidapi.com',
@@ -18,11 +23,13 @@ const Weapon = ({match}) => {
                 console.log("data not available");
             }
             else{
-                setImage(response.data.spread)
+                setWeapon(response.data)
             }
+            setIsLoading(false)
             console.log(response);
         })
         .catch((err) => {
+            setIsLoading(false)
             console.log(err);
         })
 
@@ -31,8 +38,59 @@ const Weapon = ({match}) => {
 
     return (
         <>
-            <img src={"https://valorant-api-project.herokuapp.com/images/judge"} />
-            {/* <Wallpaper title="Weapons" image={image}/> */}
+            {isLoading? <Loader /> :
+            <>
+
+
+            <Wallpaper  image={OmenWallpaper}><h1>{match.params.weapon}</h1></Wallpaper>
+            <Section>
+                <div className="container">
+                    <table className="table table-bordered table-dark table-striped shadow">
+                        <tbody>
+                            <tr className={''}>
+                                <th scope="col" className={``}>NAME</th>
+                                <td scope="col" className={``}>{weapon.name}</td>
+                            </tr>
+                            <tr className={''}>
+                                <th scope="col" className={``}>COST</th>
+                                <td scope="col" className={``}>{weapon.cost}</td>
+                            </tr>
+                            <tr className={''}>
+                                <th scope="col" className={``}>FIRE TYPE</th>
+                                <td scope="col" className={``}>{weapon.fire_type}</td>
+                            </tr>
+                            <tr className={''}>
+                                <th scope="col" className={``}>FIRE RATE</th>
+                                <td scope="col" className={``}>{weapon.fire_rate}</td>
+                            </tr>
+                            <tr className={''}>
+                                <th scope="col" rowSpan="3"style={{verticalAlign: 'center'}}>DAMAGE</th>
+                                <td scope="col" className={``}>
+                                    <table className="table table-bordered table-dark table-striped shadow">
+                                        {weapon.damage? <tbody>
+                                            <tr>
+                                                <td scope="col">Head:</td>
+                                                <td scope="col">{weapon.damage[0]?.head}</td>
+                                            </tr>
+                                            <tr>
+                                                <td scope="col">Body:</td>
+                                                <td scope="col"> {weapon.damage[0]?.body}</td>
+                                            </tr>
+                                            <tr>
+                                                <td scope="col">Legs:</td>
+                                                <td scope="col">{weapon.damage[0].legs}</td>
+                                            </tr>
+                                        </tbody>: <></>}
+                                    </table>
+                                      
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </Section>
+            </>
+            }
         </>
     )
 }
